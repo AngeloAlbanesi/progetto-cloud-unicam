78870605534ff2be13fc2a8596c4be1769433e03
AngeloAlbanesi <angeloalbanesi99@gmail.com>
2025-04-03 16:51:47 +0200

    Riorganizzazione del progetto: rimosso FunzioniDatiHotel e aggiunto FunzioniDatiHotell con nuove classi e riferimenti

---
+++ FunzioniDatiHotell/FunzioneInterrogazioneDati.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using FunzioniDatiHotell.Modelli;
+
+namespace FunzioniDatiHotel
+{
+    public class FunzioneInterrogazioneDati
+    {
+        public static async Task<Hotel[]> DaiElencoHotel()
+        {
+            string BaseUri =
+                "http://www.datiopen.it//ODataProxy/MdData('8fbb00cb-943e-4141-8e7e-758d6c292778@datiopen')/DataRows?$skip=0&$top=2000";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(BaseUri);
+            string contents = await response.Content.ReadAsStringAsync();
+            XNamespace d = "http://schemas.microsoft.com/ado/2007/08/dataservices";
+            XNamespace m = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata";
+            XNamespace atom = "http://www.w3.org/2005/Atom"; // Namespace di Atom per l'elemento "entry"
+
+            XDocument xml = XDocument.Parse(contents);
+            List<Hotel> hotels = new List<Hotel>();
+
+            foreach (var entry in xml.Descendants(atom + "entry"))
+            {
+                var contentElement = entry.Element(atom + "content");
+                if (contentElement != null)
+                {
+                    var properties = contentElement.Element(m + "properties");
+                    if (properties != null)
+                    {
+                        hotels.Add(
+                            new Hotel
+                            {
+                                Comune =
+                                    properties.Element(d + "Cccomune_608711150")?.Value
+                                    ?? "Sconosciuto",
+                                DenominazioneStruttura =
+                                    properties.Element(d + "Ccdenominazione1291548260")?.Value
+                                    ?? "Sconosciuto",
+                                Tipologia =
+                                    properties.Element(d + "Cctipologia_alb1238256790")?.Value
+                                    ?? "Sconosciuto",
+                                Categoria =
+                                    properties.Element(d + "Cccategoria_709124048")?.Value
+                                    ?? "Sconosciuto",
+                                Indirizzo =
+                                    properties.Element(d + "Ccindirizzo_1322096747")?.Value
+                                    ?? "Sconosciuto",
+                                Frazione =
+                                    properties.Element(d + "Ccfrazione_182197691")?.Value
+                                    ?? "Sconosciuto",
+                                CAP =
+                                    properties.Element(d + "Cccap_3047567")?.Value ?? "Sconosciuto",
+                                Telefono =
+                                    properties.Element(d + "Cctelefono_90134615")?.Value
+                                    ?? "Sconosciuto",
+                                FAX =
+                                    properties.Element(d + "Ccfax_3050458")?.Value ?? "Sconosciuto",
+                                IndirizzoPostaElettronica =
+                                    properties.Element(d + "Ccindirizzo_pos1351423458")?.Value
+                                    ?? "Sconosciuto",
+                                TipologiaServizio =
+                                    properties.Element(d + "Cctipologia_serv481601159")?.Value
+                                    ?? "Sconosciuto",
+                                Altitudine =
+                                    properties.Element(d + "Ccaltitudine_1110206336")?.Value
+                                    ?? "Sconosciuto",
+                                NumeroPostiletto =
+                                    properties.Element(d + "Ccnumero_posti_1304069088")?.Value
+                                    ?? "Sconosciuto",
+                            }
+                        );
+                    }
+                    else
+                    {
+                        Console.WriteLine("Properties non trovate!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Content non trovato!");
+                }
+            }
+
+            return hotels.ToArray();
+        }
+    }
+}
---
+++ FunzioniDatiHotell/Modelli/Hotel.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunzioniDatiHotell.Modelli
+{
+    
+    public class Hotel
+    {
+        public string Comune { get; set; }
+        public string Categoria { get; set; }
+
+        public string DenominazioneStruttura { get; set; }
+        public string Indirizzo { get; set; }
+        public string Frazione { get; set; }
+        public string CAP { get; set; }
+        public string Telefono { get; set; }
+        public string FAX { get; set; }
+        public string IndirizzoPostaElettronica { get; set; }
+
+        public string Tipologia { get; set; }
+
+        public string TipologiaServizio { get; set; }
+
+        public string Altitudine { get; set; }
+
+        public string NumeroPostiletto { get; set; }
+
+    }
+
+
+}
